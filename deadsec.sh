d6d2f2772cd7bdbb51ca239e69dfc3af74cc84cc
Mrx <90603785+official-hack4peace@users.noreply.github.com>
2022-10-03 13:50:31 +0530

    Create deadsec.sh

---
+++ deadsec.sh
@@ -0,0 +1,23 @@
+# color
+grn="\e[92m"
+# ASCII
+echo -e "$grn   .AMMMMMMMMMMA.
+              .AV. :::.:.:.::MA.
+             A' :..        : .:`A
+            A'..              . `A.
+          A' :.    :::::::::  : :`A
+          M  .    :::.:.:.:::  . .M
+          M  :   ::.:.....::.:   .M
+          V : :.::.:........:.:  :V
+         A  A:    ..:...:...:.   A A
+       .V  MA:.....:M.::.::. .:AM.M
+      A'  .VMMMMMMMMM:.:AMMMMMMMV: A
+     :M .  .`VMMMMMMV.:A `VMMMMV .:M:
+      V.:.  ..`VMMMV.:AM..`VMV' .: V
+        V.  .:. .....:AMMA. . .:. .V
+         VMM...: ...:.MMMM.: .: MMV
+            `VM: . ..M.:M..:::M'
+             `M::. .:.... .::M
+              M:.  :. .... ..M
+              V:  M:. M. :M .V
+              `V.:M.. M. :M.V'"
